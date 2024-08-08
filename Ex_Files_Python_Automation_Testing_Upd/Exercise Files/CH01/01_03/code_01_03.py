10f79c761b1cc63e9c6ce508751b62b2ed07eb5c
dbayles668 <dbayles@sympatico.ca>
2024-08-08 16:19:45 -0400

    correction, script uses outdated code and is missing import

--- Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH01/01_03/code_01_03.py
+++ Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH01/01_03/code_01_03.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 
-elem = driver.find_element_by_name("q")
+elem = driver.find_element(By.ID, "id-search-field")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
