b3c314a4efd3e2486c956a9e6a10e8a66a81f288
dbayles668 <dbayles@sympatico.ca>
2024-08-08 21:27:26 -0400

    corrected code

--- Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH04/04_03/code_04_03.py
+++ Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH04/04_03/code_04_03.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver= webdriver.Firefox()
 
 driver.implicitly_wait(10)
 
 driver.get('http://www.python.org')
-myDynamicElement = driver.find_element_by_id('start-shell')
+myDynamicElement = driver.find_element(By.ID,'start-shell')
 
 driver.close()
