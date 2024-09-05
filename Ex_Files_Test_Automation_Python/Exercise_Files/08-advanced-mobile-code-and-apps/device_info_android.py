e42ab6ccd174117a7601eb8a182fccb022e8436c
dbayles668 <dbayles@sympatico.ca>
2024-09-05 14:22:43 -0400

    lesson 8

---
+++ Ex_Files_Test_Automation_Python/Exercise_Files/08-advanced-mobile-code-and-apps/device_info_android.py
@@ -0,0 +1,19 @@
+from os import path
+from appium import webdriver
+
+CUR_DIR = path.dirname(path.abspath(__file__))
+APP = path.join(CUR_DIR, 'TheApp.apk')
+APPIUM = 'http://localhost:4723'
+CAPS = {
+    'platformName': 'Android',
+    'platformVersion': '10.0',
+    'deviceName': 'Android Emulator',
+    'automationName': 'UiAutomator2',
+    'app': APP,
+}
+
+driver = webdriver.Remote(APPIUM, CAPS)
+try:
+    print(driver.execute_script("mobile: deviceInfo"))
+finally:
+    driver.quit()
