16d6dcc62b88f885a0e3a27c152af0d1588c0023
dbayles668 <dbayles@sympatico.ca>
2024-09-05 11:40:59 -0400

    source android code

---
+++ Ex_Files_Test_Automation_Python/Exercise_Files/07-mobile-code-and-apps/source_android.py
@@ -0,0 +1,22 @@
+import time
+from os import path
+from appium import webdriver
+
+CUR_DIR = path.dirname(path.abspath(__file__))
+APP = path.join(CUR_DIR, 'TheApp.apk')
+APPIUM = 'http://localhost:4723'
+CAPS = {
+    'platformName': 'Android',
+    'platformVersion': '10.0',
+    'deviceName': 'Android Emulator',
+    'automationName': 'UiAutomator2',
+    'app': APP,
+}
+
+
+driver = webdriver.Remote(APPIUM, CAPS)
+try:
+    time.sleep(4)
+    print(driver.page_source)
+finally:
+    driver.quit()
