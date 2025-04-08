5aec32ee8ed0c7cf0f242d390e31a2c22947a3bf
dbayles668 <dbayles@sympatico.ca>
2025-04-08 18:44:55 -0400

    clear

--- Ex_Files_React_EssT/Exercise_Files/Ch02/02_02/02_02b/react-project/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
-const animals = ["giraffe","wolf","rhino"];
-
-createRoot(document.getElementById("root")).render(<App />);
