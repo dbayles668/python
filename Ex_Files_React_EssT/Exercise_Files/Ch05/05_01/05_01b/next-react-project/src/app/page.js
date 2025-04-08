57dfc89c9789061c40c750e45085833064d40d5f
dbayles668 <dbayles@sympatico.ca>
2025-04-08 17:41:55 -0400

    ch 5

---
+++ Ex_Files_React_EssT/Exercise_Files/Ch05/05_01/05_01b/next-react-project/src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p24">
+        <h1>More to come</h1>
+    </main>
+  );
+}
