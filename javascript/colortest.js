0872da0bda645221370889a6e32e9d303175f5ec
dbayles668 <dbayles@sympatico.ca>
2025-04-11 12:12:48 -0400

    typo

--- javascript/colortest.js
+++ javascript/colortest.js
@@ -12,21 +12,21 @@ function NumConvert(num,radix){
 // takes substring of h, length n, starting at comp
 function HexSplit(h,n,comp) {
     hex_comp = h.substring(comp, comp+n);
-    hex_conv = NumConvert(hex_comp,16)
+    hex_conv = NumConvert(hex_comp,16);
     return hex_conv;
 }
 
 function AvgClr(clra, clrb){
 //slice each input string into its components, and convert from string to integer
 
-int_one = [HexSplit(clra,2,0), HexSplit(clra,2,2), HexSplit(clra,2,4)];
-int_two = [HexSplit(clrb,2,0), HexSplit(clrb,2,2), HexSplit(clrb,2,4)];
+    int_one = [HexSplit(clra,2,0), HexSplit(clra,2,2), HexSplit(clra,2,4)];
+    int_two = [HexSplit(clrb,2,0), HexSplit(clrb,2,2), HexSplit(clrb,2,4)];
 
-//average color uses above variables, converts each component back to hexidecimal, and returns value
-// in case of odd sum, round up to next integer
+    //average color uses above variables, converts each component back to hexidecimal, and returns value
+    // in case of odd sum, round up to next integer
 
-avgcolor = [Math.round((int_one[0]+int_two[0])/2), Math.round((int_one[1]+int_two[1])/2), Math.round((int_one[2]+int_two[2])/2)];
-return avgcolor[0].toString(16)+avgcolor[1].toString(16)+avgcolor[2].toString(16);
+    avgcolor = [Math.round((int_one[0]+int_two[0])/2), Math.round((int_one[1]+int_two[1])/2), Math.round((int_one[2]+int_two[2])/2)];
+    return avgcolor[0].toString(16)+avgcolor[1].toString(16)+avgcolor[2].toString(16);
 }
 
 color_a = 'df744c'; // lavender
